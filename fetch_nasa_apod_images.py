27993aaec76421083605ec97a68bc559856a0a8f
Dtlama <serebryakovss1901@gmail.com>
2023-03-04 16:35:57 +0300

    Create fetch_nasa_apod_images.py

---
+++ fetch_nasa_apod_images.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from main import download_image
+from main import get_file_extension
+import requests
+import os
+
+
+def fetch_nasa_apod(key):
+    url = "https://api.nasa.gov/planetary/apod"
+    payload = {"api_key": key, "count": 30}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    links = response.json()
+    for number, image in enumerate(links):
+        if image["media_type"] != "image":
+            continue
+        file_name = f'nasa_apod{number}.{get_file_extension(image["url"])}'
+        file_path = os.path.join('images', file_name)
+        download_image(image["url"], file_path)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    key = os.environ["API_KEY"]
+    os.makedirs('images', exist_ok=True)
+    fetch_nasa_apod(key)
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import os
 import datetime
 
 
-load_dotenv()
-
 def get_file_extension(url):
     parsed_url = urlparse(url)
     split_text = os.path.splitext(parsed_url.path)
@@ -18,16 +16,6 @@ def download_image(url, file_path, payload=None):
 
     with open(file_path, 'wb') as file:
         file.write(response.content)
-
-def fetch_spacex():
-    url = 'https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384'
-    response = requests.get(url)
-    response.raise_for_status()
-    links = response.json()['links']['flickr']['original']
-    for number, link in enumerate(links):
-        file_name = f'spacex_photo{number}.jpg'
-        file_path = os.path.join('images', file_name)
-        download_image(link, file_path)
 
 def fetch_nasa_apod(key):
     url = "https://api.nasa.gov/planetary/apod"
@@ -59,8 +47,8 @@ def fetch_epic_photos(key):
 
 
 if __name__ == '__main__':
+    load_dotenv()
     os.makedirs('images', exist_ok=True)
-    fetch_spacex()
     get_file_extension('https://apod.nasa.gov/apod/image/2302/JWSTMIRI_ngc1365_1024.png')
 
     key = os.environ["API_KEY"]
