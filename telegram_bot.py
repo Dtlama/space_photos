4ea1f00577503ef10ffc4e5eec470d2b2291b5d8
Dtlama <serebryakovss1901@gmail.com>
2023-03-11 16:44:31 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,4 +9,6 @@ if __name__ == '__main__':
     tg_chat_id = os.environ["TG_CHAT_ID"]
     bot = telegram.Bot(token=tg_bot_token)
     bot.send_message(chat_id=tg_chat_id, text="I'm gayming")
+    with open('images/spacex_photo0.jpg', 'rb') as file:
+        bot.send_photo(chat_id=tg_chat_id, photo=file)
     print(bot.get_me())
