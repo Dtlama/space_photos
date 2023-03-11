868bf3d82744969a22ca95c457ad8aa6327b2e4c
Dtlama <serebryakovss1901@gmail.com>
2023-03-11 15:16:54 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,10 +1,11 @@
+from dotenv import load_dotenv
 from image_tools import download_image
 import requests
 import os
 
 
-def fetch_spacex():
-    url = 'https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384'
+def fetch_spacex(spacex_launch_id):
+    url = f'https://api.spacexdata.com/v5/launches/{spacex_launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     links = response.json()['links']['flickr']['original']
@@ -15,5 +16,7 @@ def fetch_spacex():
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    spacex_launch_id = os.getenv('SPACEX_LAUNCH_ID', '5eb87d42ffd86e000604b384')
     os.makedirs('images', exist_ok=True)
-    fetch_spacex()
+    fetch_spacex(spacex_launch_id)
