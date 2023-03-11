d3ce13bb3499906115aa87bd7833266061624ca5
Dtlama <serebryakovss1901@gmail.com>
2023-03-11 16:57:38 +0300

    add delay

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
+from time import sleep
 import telegram
+import random
 import os
 
 
@@ -7,8 +9,12 @@ if __name__ == '__main__':
     load_dotenv()
     tg_bot_token = os.environ["TG_BOT_TOKEN"]
     tg_chat_id = os.environ["TG_CHAT_ID"]
+    delay = os.getenv('DELAY', '14400')
     bot = telegram.Bot(token=tg_bot_token)
-    bot.send_message(chat_id=tg_chat_id, text="I'm gayming")
-    with open('images/spacex_photo0.jpg', 'rb') as file:
-        bot.send_photo(chat_id=tg_chat_id, photo=file)
-    print(bot.get_me())
+    filesindir = os.listdir('images')
+    while True:
+        file_name = random.choice(filesindir)
+        file_path = os.path.join('images', file_name)
+        with open(file_path, 'rb') as file:
+            bot.send_photo(chat_id=tg_chat_id, photo=file)
+        sleep(int(delay))
